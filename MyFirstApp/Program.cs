08819ce33431bd03335097587583296b61900174
Yudiz-Hani <113348480+Yudiz-Hani@users.noreply.github.com>
2022-10-14 14:29:52 +0530

    Add files via upload

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a = 0, b = 1, c;
+            Console.WriteLine("Enter The Number till that you want to print fibonaccie: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Series are as below: ");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 2; i < n; i++)
+            {
+                c=a+b;
+                Console.WriteLine($"{c}");
+                a=b;
+                b=c;
+            }
+        }
+    }
+}
